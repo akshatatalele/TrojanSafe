bf9d9956c14e74137bc30cea2efcd036b90b9d6b
vaishnaviK06 <kotawarvaishnavi@gmail.com>
2021-04-10 15:33:47 -0700

    Update DB

--- schema.sql
+++ schema.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS crime_data;
 CREATE TABLE crime_data (
     crime_id INTEGER PRIMARY KEY AUTOINCREMENT,
     crime_title TEXT NOT NULL,
-    crime_alert_date NUMERIC NOT NULL,
+    crime_alert_date TEXT NOT NULL,
     crime_location TEXT NOT NULL,
     crime_incident_description TEXT NOT NULL,
-    crime_timeStamp NUMERIC NOT NULL,
-    crime_lat REAL NOT NULL,
-    crime_long REAL NOT NULL
+    crime_timeStamp TEXT NOT NULL,
+    crime_lat TEXT NOT NULL,
+    crime_long TEXT NOT NULL
 );
