f1513f841dd20b93f2410c462e190681277eab78
vaishnaviK06 <kotawarvaishnavi@gmail.com>
2021-04-10 20:31:01 -0700

    Addition of lat long co-ordinates into the db

--- webScrapping.py
+++ webScrapping.py
@@ -11,11 +11,39 @@ def get_connection():
 
 def update_location_coordinates(connection):
     connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.027906453327,-118.2749037790", 1))
-    connection.execute("Update crime_data set crime_lat = ?, crime_long = ? where crime_id = ?", ("34.03430468199586,-118.28400709568707", 2))
-    connection.execute("Update crime_data set crime_lat = ?, crime_long = ? where crime_id = ?", ("34.024850673098236,-118.28176814003838", 3))
-    connection.execute("Update crime_data set crime_lat = ?, crime_long = ? where crime_id = ?", ("34.03311208160461,-118.27945309822908", 4))
-    connection.execute("Update crime_data set crime_lat = ?, crime_long = ? where crime_id = ?", ("34.02224681607721,-118.29167836920976", 5))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.03430468199586,-118.28400709568707", 2))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.024850673098236,-118.28176814003838", 3))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.03311208160461,-118.27945309822908", 4))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02224681607721,-118.29167836920976", 5))
 
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02753252915366,-118.28508509662556", 6))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02841583614427,-118.28397669961177", 7))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.0280902530464,-118.27917680787786", 8))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02521101686115,-118.2780044672892", 9))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02184862398057,-118.28023553542654", 10))
+
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02964608601271,-118.28744337485861", 11))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("33.67254330951706,-117.972303150867", 12))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.023907979689774,-118.30022200835289", 13))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02545135250305,-118.28344519430684", 14))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.025425168142945,-118.28344479014247", 15))
+
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.01858436088644,-118.28337755832518", 16))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.027487282376676,-118.30891797366498", 17))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.01847184158187,-118.2923165448302", 18))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02453411362622,-118.2812628159949", 19))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02733874040611,-118.289105258325", 20))
+    
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.03299626362487,-118.28199000732457", 21))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.01810874230469,-118.2944354448302", 22))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02844546327014,-118.28422864482994", 23))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.06648564328661,-118.20198226353455", 24))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02819178643076,-118.2758524072807", 25))
+
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.033671351114386,-118.28434963756283", 26))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.0188293865494,-118.28269509116915", 27))
+    connection.execute("Update crime_data set crime_location_coordinate = ? where crime_id = ?", ("34.02747501645694,-118.2866917313349", 28))
+    
 
 def get_crime_details(url_string, crime_nos, connection):
     
